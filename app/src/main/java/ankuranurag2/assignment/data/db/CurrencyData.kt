fc04b16d24e3d60eafe3da01c72f7a700175c3f0
ankuranurag2 <ankuranurag2@gmail.com>
2020-03-11 18:25:31 +0530

    PrimaryKey in Entity class

--- app/src/main/java/ankuranurag2/assignment/data/db/CurrencyData.kt
+++ app/src/main/java/ankuranurag2/assignment/data/db/CurrencyData.kt
@@ -1,13 +1,20 @@
 package ankuranurag2.assignment.data.db
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
 /**
  * Other fields have been ignored from the API response, as they are not being used anywhere
  */
+
+/**
+ * Currency has been made primary, so when new data arrives, previous gets replaced.
+ * See, the conflictStrategy in {CurrencyDao.insertAll()}
+ */
 @Entity(tableName = "currency_data")
 data class CurrencyData(
+    @PrimaryKey
     @SerializedName("Currency")
     val currency: String?,
     @SerializedName("CurrencyLong")
